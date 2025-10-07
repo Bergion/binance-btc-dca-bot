4bd4b5b6fdfeee5953ce5bb9a02571794c4af177
Bergion <dima.vintsyk@gmail.com>
2025-10-07 18:31:29 +0300

    Add logging

--- internal/trading/dca/dca.go
+++ internal/trading/dca/dca.go
@@ -16,10 +16,17 @@ func NewExecutor(cfg Config, binanceClient *binance.Client) *Executor {
 }
 
 func (d *Executor) Execute() {
+	slog.Info("Executing DCA")
 	tickerStat, err := d.binanceClient.GetTickerStat(d.config.Symbol)
 	if err != nil {
 		slog.Error("Failed to get ticker stat: ", slog.Any("error", err))
 	}
+
+	slog.Info(
+		"Ticker stat",
+		slog.Float64("price_change_percentage", tickerStat.PriceChangePercentage()),
+		slog.Float64("last_price", tickerStat.LastPrice()),
+	)
 
 	quantityUSDT := d.config.QuantityUSDT
 
@@ -28,6 +35,8 @@ func (d *Executor) Execute() {
 	}
 
 	quantityBTC := quantityUSDT / tickerStat.LastPrice()
+
+	slog.Info("Placing buy order", slog.Float64("quantity_btc", quantityBTC))
 
 	err = d.binanceClient.PlaceBuyOrder(d.config.Symbol, quantityBTC)
 	if err != nil {
